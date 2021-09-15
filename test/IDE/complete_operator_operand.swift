48f02f1d050eba5f75e19c99b3e7243e72e19515
Holly Borla <hborla@apple.com>
2021-09-14 20:43:55 -0700

    [Code Completion] Update a code completion test to reflect operator changes.
    This commit is temporary; we need to migrate completions inside sequence
    expressions over to the solver-based completion, because code completion
    no longer knows how to gather overloads from operators that are represented
    as UnresolvedDeclRefExprs in the AST.

--- test/IDE/complete_operator_operand.swift
+++ test/IDE/complete_operator_operand.swift
@@ -15,7 +15,9 @@ func test_binary1(c1: C1, c2: C2) {
 // BINARY_RHS_1: Begin completion
 // BINARY_RHS_1-NOT: Decl[LocalVar]/Local/TypeRelation[Identical]: c1[#C1#]; name=c1
 // BINARY_RHS_1-DAG: Decl[LocalVar]/Local/TypeRelation[Identical]: c2[#C2#]; name=c2
+}
 
+func test_binary2(c1: C1, c2: C2) {
   _ = c2 <--> #^BINARY_RHS_2^#
 // BINARY_RHS_2: Begin completion
 // BINARY_RHS_2-NOT: Decl[LocalVar]/Local/TypeRelation[Identical]: c2[#C2#]; name=c2
@@ -26,8 +28,10 @@ prefix func --> (operand: C1) -> Bool {}
 prefix func --> (operand: C2) -> Bool {}
 
 func test_prefix1(c1: C1, c2: C2) {
+// FIXME: completions in operator expressions need to be migrated to
+// solver-based completion.
   _ = -->#^PREFIX_1^#
 // PREFIX_1: Begin completion
-// PREFIX_1-DAG: Decl[LocalVar]/Local/TypeRelation[Identical]: c1[#C1#]; name=c1
-// PREFIX_1-DAG: Decl[LocalVar]/Local/TypeRelation[Identical]: c2[#C2#]; name=c2
+// PREFIX_1-DAG: Decl[LocalVar]/Local: c1[#C1#]; name=c1
+// PREFIX_1-DAG: Decl[LocalVar]/Local: c2[#C2#]; name=c2
 }
